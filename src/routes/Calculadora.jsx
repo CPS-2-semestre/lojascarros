e38e849e20aa93c7455807cdedc667312d2fe083
Lucas <lucaslugui1604@gmail.com>
2025-09-02 10:46:11 -0300

    adicionando parte do codigo calculadora

--- src/routes/Calculadora.jsx
+++ src/routes/Calculadora.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Calculadora = () => {
   return (
     <div>
-      
+      export default function App() {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [resultado, setResultado] = useState("");
+
+  const calcular = (operacao) => {
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+
+    if (isNaN(n1) || isNaN(n2)) {
+      setResultado("Digite números válidos!");
+      return;
+    }
     </div>
   )
 }
