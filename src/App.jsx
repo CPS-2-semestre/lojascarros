c2c96ed7c09a44fabcdaad155264db134630a084
Eduardo Delarissia <antonio.delarissia@gmail.com>
2025-09-02 10:11:43 -0300

    importando

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Calculadora from "./routes/Calculadora"
+import Sobre from "./routes/Sobre"
+import Carros from "./routes/Carros"
+import Home from "./routes/Home"
 function App() {
  
 
   return (
     <>
+    
      
     </>
   )
