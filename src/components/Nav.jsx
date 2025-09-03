89e82e88b2cb250cc8bf2cbbd29f69644295283c
Joao <joao34palmeira.28@gmail.com>
2025-09-03 11:39:27 -0300

    Ajustando logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,21 @@ import Carros from '../routes/Carros'
 import Error from '../routes/Error'
 import Home from '../routes/Home'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 const Nav = () => {
   return (
     <div>
       <header className="bg-black text-white p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Loja carrões</h1>
+        {/* Logo e Título Alinhados à Esquerda */}
+        <div className="flex items-center space-x-2"> 
+          <img 
+            src={logo}  // Usando a variável logo diretamente
+            alt="Logo"
+            className="h-10"  // Ajuste o tamanho do logo conforme necessário
+          />
+          <h1 className="text-2xl font-bold">Loja Carrões</h1>
+        </div>
         <nav className="space-x-4">
           <Link to="/" className="hover:text-red-400">Home</Link>
           <Link to="/carros" className="hover:text-red-400">Carros</Link>
